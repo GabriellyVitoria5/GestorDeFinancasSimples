c54e3fdf82ba99a04b51f5eb56998f8bb792e522
GabriellyVitoria <assuncaogabrielly5@gmail.com>
2024-10-13 20:55:02 -0300

    criando tela

--- src/gestordefinancas/GestorDeFinancas.java
+++ src/gestordefinancas/GestorDeFinancas.java
@@ -1,20 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package gestordefinancas;
 
-/**
- *
- * @author assun_p3feroz
- */
 public class GestorDeFinancas {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
